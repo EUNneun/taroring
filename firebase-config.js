4969ff6de696ebceef87b08ab8953729531430db
EUNneun <101326692+EUNneun@users.noreply.github.com>
2026-09-16 17:07:22 +0900

    chore: add Firebase web config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정에서 값을 복사하세요.
+// Firebase Web config는 브라우저에 공개되는 식별 정보이며, 실제 접근 권한은 Firestore Rules로 통제합니다.
+window.TARORING_FIREBASE_CONFIG = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
