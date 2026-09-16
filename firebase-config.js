4367d692deda94145457adf0353b2d387142246f
EUNneun <101326692+EUNneun@users.noreply.github.com>
2026-09-16 17:36:13 +0900

    feat: connect TarotRing Firebase web app

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정에서 값을 복사하세요.
 // Firebase Web config는 브라우저에 공개되는 식별 정보이며, 실제 접근 권한은 Firestore Rules로 통제합니다.
 window.TARORING_FIREBASE_CONFIG = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyCGh7C163jQaif-SdZlnCr6dqkykdHeMFg',
+  authDomain: 'tarotring-f98bb.firebaseapp.com',
+  projectId: 'tarotring-f98bb',
+  storageBucket: 'tarotring-f98bb.firebasestorage.app',
+  messagingSenderId: '607985650061',
+  appId: '1:607985650061:web:4ec5c3540f01c15a0b95c4',
+  measurementId: 'G-0NNSV6DKGV'
 };
